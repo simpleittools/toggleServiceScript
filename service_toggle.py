fbcd5d4e2f45df5dc270005aeeeeb6d74268e51c
icedvigilante <wm.ryan.mooney@gmail.com>
2022-11-20 18:18:00 -0900

    corrected name shadowing

--- service_toggle.py
+++ service_toggle.py
@@ -8,14 +8,14 @@ displayName = "PostgreSQL"
 
 # get_service will find if the service you are looking for is available
 def get_service(name):
-    service = None
+    service_check = None
     try:
-        service = psutil.win_service_get(name)
-        service = service.as_dict()
+        service_check = psutil.win_service_get(name)
+        service_check = service_check.as_dict()
     except Exception as err:
         print(str(err))
 
-    return service
+    return service_check
 
 
 service = get_service(serviceName)
@@ -24,13 +24,13 @@ default = "y"
 
 # check service status and communicate to the user
 if service and service['status'] == 'running':
-    confirm = input(f'The {displayName} service is running. Do you want to STOP? \nY/n') or default
+    confirm = input(f'The {displayName} service is running. Do you want to STOP? (Y/n) \n') or default
     if confirm.lower() == "y" or confirm == '':
         os.system(f'Elevate.exe net stop {serviceName}')
     else:
         quit()
 else:
-    confirm = input(f'The {displayName} service not is running. Do you want to START? \nY/n') or default
+    confirm = input(f'The {displayName} service not is running. Do you want to START? (Y/n) \n') or default
     if confirm.lower() == "y" or confirm == '':
         os.system(f'Elevate.exe net start {serviceName}')
     else:
